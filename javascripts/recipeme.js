b7837e190ab5e79b5a68ffdeb1197b5594d0fb7d
squashingtondc <squashingtondc@gmail.com>
2014-05-21 01:24:06 -0500

    Update recipeme.js

--- javascripts/recipeme.js
+++ javascripts/recipeme.js
@@ -29,7 +29,7 @@ function IngredientListViewModel() {
     self.getRecipes = function () {
         // Load initial state from server, convert it to Task instances, then populate self.tasks
         var newUrl = "http://food2fork.com/api/search?key=b49f1a7e472550dd4fa5d08d735db081&q=shredded%20chicken";
-        var urlTest = "http://api.yummly.com/v1/api/recipes?_app_id=246a2dfe&_app_key=575d5e01b0e7c25ab538e27743dc15db&q=onion+soup&callback=myCallback";
+        var urlTest = "http://api.yummly.com/v1/api/recipes?_app_id=246a2dfe&_app_key=575d5e01b0e7c25ab538e27743dc15db&q=onion+soup&callback=?";
         var url = "http://api.yummly.com/v1/api/recipes?_app_id=246a2dfe&_app_key=575d5e01b0e7c25ab538e27743dc15db";
         if (self.ingredients().length > 0) {
             for (var i = 0; i < self.ingredients().length; i++) {
